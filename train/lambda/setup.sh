2e5b88510a06de8e663e4a8530f4d77cb7f2df81
napatswift <napat.do@ku.th>
2023-05-10 23:33:47 +0700

    add scripts

---
+++ train/lambda/setup.sh
@@ -0,0 +1,34 @@
+# Install Anaconda
+cp /tmp
+curl --output anaconda.sh https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
+bash anaconda.sh
+# After installed
+source ~/.bashrc
+
+conda create --name openmmlab python=3.8 -y
+conda activate openmmlab
+conda install pytorch torchvision -c pytorch
+
+# Install mmocr
+pip install -U openmim
+mim install mmengine
+mim install mmcv
+mim install mmdet
+
+git clone https://github.com/napatswift/mmocr.git
+cd mmocr
+pip install -v -e .
+
+# Download the thvl and textdet-thvote datasets from GitHub.
+wget https://github.com/napatswift/vote-count/releases/download/v0.0.1/vl+vc-textdet.tar.gz
+wget https://github.com/napatswift/syTH-doc/releases/download/v0.0.1/textdet-thvote.tar.gz
+
+DATA_DIR="data/det/"
+# creates a directory called `data/det/`, if not exist
+if [ ! -d "$DATA_DIR" ]; then
+    mkdir -p "$DATA_DIR"
+fi
+
+# extracts dataset to the `data/det/` directory
+tar xzf vl+vc-textdet.tar.gz -C $DATA_DIR
+tar xzf textdet-thvote.tar.gz -C $DATA_DIR
---
+++ train/runpod/setup.sh
@@ -0,0 +1,24 @@
+apt-get update
+apt-get install python3-setuptools
+apt-get install tmux
+
+pip install -U openmim
+mim install mmengine
+mim install mmcv mmdet mmocr
+
+git clone https://github.com/napatswift/mmocr.git
+cd mmocr
+
+# Download the thvl and textdet-thvote datasets from GitHub.
+wget https://github.com/napatswift/vote-count/releases/download/v0.0.2/vl+vc-textdet.tar.gz
+wget https://github.com/napatswift/syTH-doc/releases/download/v0.0.1/textdet-thvote.tar.gz
+
+DATA_DIR="data/det/"
+# creates a directory called `data/det/`, if not exist
+if [ ! -d "$DATA_DIR" ]; then
+    mkdir -p "$DATA_DIR"
+fi
+
+# extracts dataset to the `data/det/` directory
+tar xzf vl+vc-textdet.tar.gz -C $DATA_DIR
+tar xzf textdet-thvote.tar.gz -C $DATA_DIR

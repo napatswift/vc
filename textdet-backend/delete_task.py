16455e1c09ea49d20ff84634604d7da71d47f4be
napatswift <napat.do@ku.th>
2023-05-13 17:31:26 +0700

    Add image volume

---
+++ textdet-backend/delete_task.py
@@ -0,0 +1,1 @@
+"""This script contains the function to delete a task from the app."""
--- textdet-backend/mlback/model.py
+++ textdet-backend/mlback/model.py
@@ -1,8 +1,13 @@
 from label_studio_ml.model import LabelStudioMLBase
 from label_studio_ml.utils import get_image_local_path
+from label_studio_tools.core.utils.params import get_env
+from label_studio_tools.core.utils.io import get_local_path
+
 import cv2
 from gradio_client import Client
 import json
+
+model_client = Client("https://napatswift-votecount-ml-be.hf.space/")
 
 class MyModel(LabelStudioMLBase):
     def __init__(self, **kwargs):
@@ -17,19 +22,23 @@ class MyModel(LabelStudioMLBase):
             'value_key': schema['inputs'][0]['value']
         }
 
-        self.model_api = Client("https://napatswift-votecount-ml-be.hf.space/")
-    
+        self.model_api = model_client
+
     def predict(self, tasks, **kwargs):
         prediction = []
 
         image_value_key = self.schema['value_key']
         for task in tasks:
-            image_path = get_image_local_path(task['data'][image_value_key])
+            image_path = task['data'][image_value_key]
+            print('Task ID', task['id'], 'image path', image_path)
+            if image_path.startswith('/data/local-files/?d='):
+                image_path = image_path.replace('/data/local-files/?d=','/')
             
             try:
-                results = self._predict(image_path)
+                results, score = self._predict(image_path)
             except Exception as e:
                 results = []
+                score = 0.0
 
             image_height, image_width = cv2.imread(image_path).shape[:2]
 
@@ -42,12 +51,12 @@ class MyModel(LabelStudioMLBase):
                         'value': self._get_x_y_w_h(result, image_width, image_height)
                     } for result in results
                 ],
-                'score': 1.0,
+                'score': score,
                 'model_version': self.model_version
             })
-        
+
         return prediction
-    
+
     def _predict(self, image_path):
         """
         Predicts the bounding boxes of objects in an image.
@@ -57,13 +66,18 @@ class MyModel(LabelStudioMLBase):
 
         Returns:
             list: A list of bounding boxes, in the format `[x1, y1, x2, y2]`.
+            float: The score of the prediction.
 
         Raises:
             Exception: If an error occurs during prediction.
         """
 
+
+
         # Get the result file path.
-        result_file_path = self.model_api.predict(image_path, api_name="/predict")
+        print('Predicting image', image_path)
+        result_file_path = self.model_api.predict(
+            image_path, api_name="/predict")
 
         # Load the result file.
         with open(result_file_path) as fp:
@@ -72,12 +86,27 @@ class MyModel(LabelStudioMLBase):
         # Get the first prediction.
         prediction = loaded_data['predictions'][0]
 
-        # Get the bounding boxes.
-        bounding_boxes = prediction['det_polygons']
+        # Get the bounding boxes and score.
+        bounding_boxes, scores = self._prediction_filter(prediction['det_polygons'],
+                                                         prediction['det_scores'])
 
-        # Return the bounding boxes.
-        return bounding_boxes
-    
+        score = self._calc_score(scores)
+
+        # Return the bounding boxes, and score.
+        return bounding_boxes, score
+
+    def _prediction_filter(self, polygons, scores, score_threshold=0.5):
+        filtered_polygons = []
+        filtered_scores = []
+        for polygon, score in zip(polygons, scores):
+            if score >= score_threshold:
+                filtered_polygons.append(polygon)
+                filtered_scores.append(score)
+        return filtered_polygons, filtered_scores
+
+    def _calc_score(self, scores):
+        return sum(scores) / len(scores)
+
     def _get_x_y_w_h(self, polygon: list, image_width, image_height):
         """
         Get the x, y, w, and h coordinates of a rectangle from a polygon.
@@ -97,10 +126,12 @@ class MyModel(LabelStudioMLBase):
         """
 
         # Get the x-coordinates of the polygon.
-        x_coordinates = [pos/image_width for i, pos in enumerate(polygon) if i % 2 == 0]
+        x_coordinates = [pos/image_width*100 for i,
+                         pos in enumerate(polygon) if i % 2 == 0]
 
         # Get the y-coordinates of the polygon.
-        y_coordinates = [pos/image_height for i, pos in enumerate(polygon) if i % 2 != 0]
+        y_coordinates = [pos/image_height*100 for i,
+                         pos in enumerate(polygon) if i % 2 != 0]
 
         # Get the minimum and maximum x-coordinates.
         min_x = min(x_coordinates)
@@ -125,4 +156,3 @@ class MyModel(LabelStudioMLBase):
 
         # Return the rectangle.
         return rectangle
-
